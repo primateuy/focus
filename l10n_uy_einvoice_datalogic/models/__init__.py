85e5f67865c4e0a8393736f25869e14b7e0a5b6f
primateuy <diego@primate.uy>
2025-02-03 10:20:27 -0300

    datalogic en focus duplicado

--- l10n_uy_einvoice_datalogic/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import account_move
-from . import account_payment
-from . import cfe_received
-from . import einvoice_config
-from . import res_company
-from . import ir_actions_report
--- l10n_uy_einvoice_datalogic/models/res_company.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import models, fields, api
-
-class ResCompany(models.Model):
-    _inherit = 'res.company'
-
-    company_code = fields.Char(string='Código de la compañía', required=True)

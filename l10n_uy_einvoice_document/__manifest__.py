817cfb6953a6688e4c42c7ea4798839d09de17ed
Andres Iglesias <102163345+aiglesas@users.noreply.github.com>
2025-02-13 14:23:29 -0300

    Add files via upload

--- l10n_uy_einvoice_document/__manifest__.py
+++ l10n_uy_einvoice_document/__manifest__.py
@@ -1,35 +1,22 @@
 # -*- coding: utf-8 -*-
 {
     'name': "l10n_uy_einvoice_document",
-
-    'summary': "Short (1 phrase/line) summary of the module's purpose",
-
-    'description': """
-Long description of module's purpose
-    """,
-
-    'author': "My Company",
-    'website': "https://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
+    'author': 'Primate',
+    'category': 'Contabilidad',
+    'summary': 'Customización de Factura Electrónica para Uruguay',
+    'website': 'primate.uy',
+    'version': '1.0',
     'depends': ['base', 'account', 'web'],
-
-    # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
         'data/report_paperformat_data.xml',
         'templates/invoice_document_custom.xml',
         'templates/copy_web_core.xml',
         'views/res_company_view.xml',
         'views/account_move_view.xml',
     ],
-    # only loaded in demonstration mode
-    'demo': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
 }
 

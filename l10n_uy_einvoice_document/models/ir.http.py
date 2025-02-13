21b8feff414a81b75faddef77d4e87e6b5e79c78
aiglesas <aiglesias.giordano@gmail.com>
2025-02-13 14:45:43 -0300

    update

--- l10n_uy_einvoice_document/models/ir.http.py
+++
@@ -1,9 +0,0 @@
-from odoo import models
-
-class IrHttp(models.AbstractModel):
-    _inherit = 'ir.http'
-
-    @classmethod
-    def _get_translation_frontend_modules_name(cls):
-        modules = super()._get_translation_frontend_modules_name()
-        return modules + ['l10n_uy_einvoice_document']
